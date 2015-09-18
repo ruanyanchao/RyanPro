8e5d4c4e3608d368cb6b070f88b008643273ac4e
Ryan Ruan <Ryan@ruanyanchaodeiMac.local>
2015-09-18 16:25:11 +0800

    sdfsdfsdfs

---
+++ RyanProject/RyanProject/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  RyanProject
+//
+//  Created by Ryan Ruan on 15/9/18.
+//  Copyright © 2015年 Ryan Ruan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
